1a88905ef5afec8f097d1349993dbaf440783b3b
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-31 16:52:29 +0200

    Mapping JPA Collaborateur

--- src/main/java/dev/sgp/entite/Collaborateur.java
+++ src/main/java/dev/sgp/entite/Collaborateur.java
@@ -3,16 +3,48 @@ package dev.sgp.entite;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Collaborateur {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	private int id;
+
+	@Column(name = "MATRICULE", unique = true, nullable = false)
 	private String matricule;
+
+	@Column(name = "NOM", nullable = false)
 	private String nom;
+
+	@Column(name = "PRENOM", nullable = false)
 	private String prenom;
+
+	@Column(name = "DATE_NAISSANCE", nullable = false)
 	private LocalDate dateNaissance;
+
+	@Column(name = "ADRESSE", nullable = false)
 	private String adresse;
+
+	@Column(name = "NO_SECU", nullable = false)
 	private String noSecu;
+
+	@Column(name = "EMAIL_PRO", nullable = false)
 	private String emailPro;
+
+	@Column(name = "PHOTO")
 	private String photo;
+
+	@Column(name = "DATE_HEURE_CREATION", nullable = false)
 	private ZonedDateTime dateHeureCreation;
+
+	@Column(name = "ACTIF", nullable = false)
 	private boolean actif;
 
 	public String getMatricule() {
@@ -93,5 +125,13 @@ public class Collaborateur {
 
 	public void setActif(boolean actif) {
 		this.actif = actif;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 }
